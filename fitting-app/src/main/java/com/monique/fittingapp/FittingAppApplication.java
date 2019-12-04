34eed83852841d6e1c5ebe6c0b3e45a91f128f67
Monique <monique.jones712@gmail.com>
2019-12-03 22:08:59 -0600

    first commit

---
+++ fitting-app/src/main/java/com/monique/fittingapp/FittingAppApplication.java
@@ -0,0 +1,13 @@
+package com.monique.fittingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FittingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FittingAppApplication.class, args);
+	}
+
+}
---
+++ fitting-app/src/test/java/com/monique/fittingapp/FittingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monique.fittingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FittingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
